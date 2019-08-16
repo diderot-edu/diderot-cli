765b8722ba47472c3826baded8cd60da9e2cfbc4
Rohan Yadav <rohany@alumni.cmu.edu>
2019-08-16 10:08:46 -0400

    Custom argparse formatter

--- diderot_core.py
+++ diderot_core.py
@@ -16,6 +16,29 @@ if requests_spec is None:
 
 import requests
 
+# custom formatter for argparse
+class Formatter(argparse.HelpFormatter):
+    # use defined argument order to display usage
+    def _format_usage(self, usage, actions, groups, prefix):
+        if prefix is None:
+            prefix = 'usage: '
+
+        # if usage is specified, use that
+        if usage is not None:
+            usage = usage % dict(prog=self._prog)
+
+        # if no optionals or positionals are available, usage is just prog
+        elif usage is None and not actions:
+            usage = '%(prog)s' % dict(prog=self._prog)
+        elif usage is None:
+            prog = '%(prog)s' % dict(prog=self._prog)
+            # build full usage string
+            action_usage = self._format_actions_usage(actions, groups) # NEW
+            usage = ' '.join([s for s in [prog, action_usage] if s])
+            # omit the long line wrapping code
+        # prefix with 'usage:'
+        return '%s%s\n\n' % (prefix, usage)
+
 # The basic architecture of this CLI is based on usage of the cmd.Cmd
 # python package, which makes it easy to implement a REPL. The library
 # lets you implement different commands for the REPL by letting users
@@ -63,7 +86,7 @@ class DiderotCLI(cmd.Cmd):
         self.logged_in = True
 
     def create_course_parser(self, progName):
-        parser = argparse.ArgumentParser(prog=progName)
+        parser = argparse.ArgumentParser(prog=progName, formatter_class=Formatter)
         if self.course is None:
             parser.add_argument('course', help="Course label")
         return parser
