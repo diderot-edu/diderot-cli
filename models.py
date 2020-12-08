4928b0c56c22c436d9105ad14d166af1a34af124
Yura Khlyan <yskhlyan@quintagroup.com>
2020-12-08 12:52:33 +0200

    Replaced URLs

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from cli_utils import APIError, singleton_or_none
 # URL constants for API access.
 COURSE_API = "/frontend-api/courses/courses/available/"
 LAB_API = "/frontend-api/courses/{}/codelabs/"
-BOOK_API = "/api/books/"
-PARTS_API = "/api/parts/"
-CHAPTERS_API = "/api/chapters/"
+BOOK_API = "/frontend-api/books/cli/"
+PARTS_API = "/frontend-api/parts/cli/"
+CHAPTERS_API = "/frontend-api/chapters/cli/"
 MANAGE_BOOK_API = "/frontend-api/courses/{course_id}/books/{book_id}/"
 SUBMIT_ASSIGNMENT_API = "/frontend-api/courses/{}/codelabs/{}/submissions/create_and_submit/"
 UPLOAD_FILES_API = "/frontend-api/courses/{}/codelabs/{}/"
